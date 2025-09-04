c058250e0e5cbfb34952a8b0477192a558441b46
Dakai-kun <nezgaming94@gmail.com>
2025-09-04 12:40:33 +0700

    adding system for product

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,11 +1,71 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::latest()->paginate(5);
+
+        return new ProductResource(true, 'List Data Category', $categories);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $data = Category::create([
+            'name' => $request->name
+        ]);
+
+        return new ProductResource(true, 'Data Berhasil Dibuat!', $data);
+    }
+
+    public function show($id)
+    {
+        $categories = Category::find($id);
+
+        return new ProductResource(true, 'Detail Data Product!', $categories);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $categories = Category::find($id);
+
+        $categories->update([
+            'name' => $request->name
+        ]);
+
+        return new ProductResource(true, 'Data Berhasil Diubah', $categories);
+    }
+
+    public function destroy($id)
+    {
+        $categories = Category::find($id);
+
+        $categories->delete();
+
+        return new ProductResource(true, 'Data Berhasil Dihapus!', $categories);
+    }
 }
--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -19,8 +21,8 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg, jpg, png|max:2048',
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
             'name' => 'required',
             'description' => 'required',
             'weight' => 'required',
@@ -30,6 +32,10 @@ class ProductController extends Controller
             'categoryId' => 'required',
             'userId' => 'required'
         ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
 
         $imageName = time() . '.' . $request->image->extension();
         $request->image->move(public_path('products'), $imageName);
@@ -46,6 +52,91 @@ class ProductController extends Controller
             'userId' => $request->userId
         ]);
 
-        return $data;
+        return new ProductResource(true, 'Data Product Bershasil Ditambahkan', $data);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        return new ProductResource(true, 'Detail Data Product!', $product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'name' => 'required',
+            'description' => 'required',
+            'weight' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'gram' => 'required',
+            'categoryId' => 'required',
+            'userId' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $product = Product::find($id);
+
+        if ($request->hasFile('image')) {
+
+            //delete old image
+
+            if (File::exists(public_path($product->image))) {
+
+                File::delete(public_path($product->image));
+
+            } else {
+
+                dd('File does not exists.');
+
+            }
+
+            //upload image
+            $imageName = time() . '.' . $request->image->extension();
+            $request->image->move(public_path('products'), $imageName);
+
+            //update product with new image
+            $product->update([
+                'image' => 'products/' . $imageName,
+                'name' => $request->name,
+                'description' => $request->description,
+                'weight' => $request->weight,
+                'price' => $request->price,
+                'stock' => $request->stock,
+                'gram' => $request->gram,
+                'categoryId' => $request->categoryId,
+                'userId' => $request->userId
+            ]);
+
+        } else {
+
+            //update product without image
+            $product->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'weight' => $request->weight,
+                'price' => $request->price,
+                'stock' => $request->stock,
+                'gram' => $request->gram,
+                'categoryId' => $request->categoryId,
+                'userId' => $request->userId
+            ]);
+        }
+
+        return new ProductResource(true, 'Data Product Berhasil Diubah!', $product);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        File::delete(public_path($product->image));
+        $product->delete();
+
+        return new ProductResource(true, 'Data Berhasil Dihapus!', $product);
     }
 }
