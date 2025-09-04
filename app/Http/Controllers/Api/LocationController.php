9f7dc3a14d3c7c94df04d444fe2fbe20105aa621
Dakai-kun <nezgaming94@gmail.com>
2025-09-04 20:56:43 +0700

    crud location

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -1,11 +1,90 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Location;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LocationController extends Controller
 {
-    //
+    public function index()
+    {
+        $locations = Location::latest()->paginate(5);
+
+        return response()->json([
+            'message' => 'List Data!',
+            'success' => 200,
+            'data' => $locations
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),422);
+        }
+
+        $data = Location::create([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'message' => 'Data Berhasil Ditambahkan!',
+            'success' => 200,
+            'data' => $data
+        ]);
+    }
+
+    public function show($id)
+    {
+        $locations = Location::find($id);
+
+        return response()->json([
+            'message' => 'Detail Data Location',
+            'success' => 200,
+            'data' => $locations
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $data = Location::find($id);
+
+        $data->update([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'message' => 'Data Berhasil Diubah!',
+            'success' => 200,
+            'data' => $data
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $data = Location::find($id);
+
+        $data->delete();
+
+        return response()->json([
+            'message' => 'Data Berhasil Dihapus!',
+            'success' => 200,
+            'data' => $data
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,15 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::apiResource('/products', ProductController::class);
-Route::apiResource('/categories', CategoryController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('/products', ProductController::class);
+
+Route::apiResource('/categories', CategoryController::class);
+
+Route::get('/location', [LocationController::class, 'index']);
+Route::post('/location/create', [LocationController::class, 'store']);
+Route::get('/location/{id}', [LocationController::class, 'show']);
+Route::post('/location/{id}', [LocationController::class, 'update']);
+Route::delete('/location/{id}', [LocationController::class, 'destroy']);
